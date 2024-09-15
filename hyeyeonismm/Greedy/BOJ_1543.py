eca90fea6575902c88c26b7e1a96477c0e779524
hyeyeonismm <hyeyeonismm@gmail.com>
2024-09-15 22:22:05 +0900

    feat: BOJ / S5 / 문서검색

---
+++ hyeyeonismm/Greedy/BOJ_1543.py
@@ -0,0 +1,12 @@
+n = input()
+word = input()
+count=0
+i=0
+while i <= len(n):
+    if word==n[i:i+len(word)]:
+        i+=len(word)
+        count+=1
+    else:
+        i+=1
+        
+print(count)
