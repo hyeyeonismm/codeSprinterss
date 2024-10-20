54cbc139cd3c0e292869a4bd56d90d6641498910
hyeyeonismm <hyeyeonismm@gmail.com>
2024-10-20 18:17:51 +0900

    feat: BOJ / S3 / 1

---
+++ ss.py
@@ -0,0 +1,11 @@
+
+while 1:
+    try:
+        n = int(input())
+        i='1'  
+        while int(i)%n!=0:
+            i+='1'
+        print(len(i))
+    except EOFError:
+        break
+        
