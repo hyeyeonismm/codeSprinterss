dff3754b914602ed68369e8b15d4c099b60c6e36
hyeyeonismm <hykim@dci.co.kr>
2025-02-23 23:33:43 +0900

    feat: BOJ / S5 / 단어 정렬

--- hyeyeonismm/implement/BOJ_1193.py
+++ hyeyeonismm/implement/BOJ_1193.py
@@ -1,27 +1,16 @@
 n = int(input())
+line = 1
 
-li = [0]
-i = 1
-tmp = 1
-while tmp < n:
-    li.append(tmp)
-    tmp = tmp + (i + 1)
-    i += 1
+while n > line:
+    n -= line
+    line += 1
 
-flag = i + 1
-
-if flag % 2 == 0:
-    mo = 0
-    za = flag
-    for i in range(li[-1], n):
-        mo += 1
-        za -= 1
+if line % 2 == 0:
+    za = n
+    mo = line - n + 1
 else:
-    za = 0
-    mo = flag
-    for i in range(li[-1], n):
-        mo -= 1
-        za += 1
+    za = line - n + 1
+    mo = n
 
 
 print(f"{za}/{mo}")
