87fba9a480d3b73dd09fae3095b50a50769b6efe
nakyong82 <nakyong7770@gmail.com>
2025-04-17 22:55:00 +0900

    feat: BOJ / S2 / 병사 배치하기

---
+++ naarang/DynamicProgramming/BOJ_18353.py
@@ -0,0 +1,35 @@
+n = int(input())
+soldiers = list(map(int, input().split(" ")))
+
+lis = [soldiers[0]]
+
+def binary_search(target):
+    start = 0
+    end = len(lis) - 1
+
+    while start < end:
+        mid = (start + end) // 2
+
+        if lis[mid] == target:
+            return mid
+        elif lis[mid - 1] < target < lis[mid]:
+            return mid
+        elif lis[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return start
+
+for i in range(n):
+    if lis[-1] < -soldiers[i]:
+        lis.append(-soldiers[i])
+    else:
+        index = binary_search(-soldiers[i])
+        lis[index] = -soldiers[i]
+
+
+print(n - len(lis))
+
+# LIS 알고리즘을 사용하자! -> 이분 탐색을 이용한 방식 O(nlogn)
+# 내림차순으로 정렬하는 코드를 작성하고 -만 붙여서 오름차순 정렬도 가능하다!
