7930ba17b73a62e5ab60a8e77e3ce573aeeb3a15
chungchae <chungchae0@gmail.com>
2025-04-27 11:09:40 +0900

    PGS/L2/가장큰수

---
+++ chungchae/sort/PGS_가장큰수.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+
+using namespace std;
+
+// PGS 가장 큰 수 (sort) (L2)
+
+bool compare(string a, string b) {
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+
+    vector<string> str_num;
+
+    for (int n : numbers) {
+        str_num.push_back(to_string(n));
+    }
+
+    sort(str_num.begin(), str_num.end(), compare);
+
+    if (str_num[0] == "0") return "0";
+
+    for (string s : str_num) {
+        answer += s;
+    }
+
+    return answer;
+}
